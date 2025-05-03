8b02f23d630fcc47040b606a47ae94bbea103d7c
Snansidansi <165903080+Snansidansi@users.noreply.github.com>
2025-05-03 10:19:49 +0200

    Fixed type of name col in users to TEXT (from VARCHAR(60))

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -3,7 +3,7 @@ CREATE TABLE users (
     id uuid NOT NULL PRIMARY KEY,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
-    name VARCHAR(60) UNIQUE NOT NULL
+    name TEXT UNIQUE NOT NULL
 );
 
 -- +goose Down
