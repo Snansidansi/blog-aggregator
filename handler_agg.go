26add6e347c76181bb7e637c44a994321698c860
Snansidansi <165903080+Snansidansi@users.noreply.github.com>
2025-05-05 12:22:13 +0200

    Updated agg command to scrape feed titles automatically after given duration.

--- handler_agg.go
+++ handler_agg.go
@@ -3,16 +3,48 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 )
 
 func handlerStartAggregator(s *state, cmd command) error {
-	const url = "https://www.wagslane.dev/index.xml"
-
-	rssFeed, err := fetchFeed(context.Background(), url)
-	if err != nil {
-		return err
+	if len(cmd.args) != 1 {
+		return fmt.Errorf("usage: %s <time-between-reqs (e.g. 1s, 1m, 1h, 1h10m, ...)>", cmd.name)
 	}
 
-	fmt.Printf("%+v", rssFeed)
+	timeBetweenReqs, err := time.ParseDuration(cmd.args[0])
+	if err != nil {
+		return fmt.Errorf("invalid duration: %v\nExample durations: 1s, 1m, 1h, 1h10m, ...", err)
+	}
+
+	fmt.Printf("Collecting feeds every: %v\n", timeBetweenReqs)
+
+	ticker := time.NewTicker(timeBetweenReqs)
+	for ; ; <-ticker.C {
+		scrapeFeeds(s)
+		fmt.Println("")
+	}
+}
+
+func scrapeFeeds(s *state) error {
+	nextFeedToFetch, err := s.db.GetNextFeedToFetch(context.Background())
+	if err != nil {
+		return fmt.Errorf("unable to next feed to fetch: %v", err)
+	}
+
+	rssFeed, err := fetchFeed(context.Background(), nextFeedToFetch.Url)
+	if err != nil {
+		return fmt.Errorf("unable to fetch the next feed: %v", err)
+	}
+
+	err = s.db.MarkFeedFetched(context.Background(), nextFeedToFetch.ID)
+	if err != nil {
+		return fmt.Errorf("unable to march next feed as fetched: %v", err)
+	}
+
+	fmt.Printf("%d titles for %s:\n", len(rssFeed.Channel.Item), nextFeedToFetch.Name)
+	for _, item := range rssFeed.Channel.Item {
+		fmt.Printf("* %s\n", item.Title)
+	}
+
 	return nil
 }
